48865fa0c9638943be558cd36b96070d8897c573
heyilias <ilias7576@gmail.com>
2021-12-21 18:23:15 +0100

    responsive

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -9,14 +9,14 @@
      </div>
 
      <div class="idea-container bg-white rounded-xl flex mt-4">
-      <div class="flex flex-1 px-4 py-6">
-         <div class="flex-none">
+      <div class="flex flex-1 flex-col md:flex-row px-4 py-6">
+         <div class="flex-none mx-4">
             <a href="#">
                <img src="https://source.unsplash.com/200x200/?face" alt="avatar" class="w-14 h-14 rounded-xl  ">
             </a>
          </div>
          
-         <div class="w-full mx-4">
+         <div class="w-full mx-2 md:mx-4">
             <h4 class="text-xl font-semibold">
                <a href="#" class="hover:underline">A random title can go here</a>
             </h4>
@@ -27,10 +27,10 @@
             </div>
             
             
-            <div class="flex items-center justify-between mt-6">
+            <div class="flex flex-col md:flex-row md:items-center justify-between mt-6">
                <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
-                  <div class="font-bold text-gray-900">John Doe</div>
-                  <div>&bull;</div>
+                  <div class="hidden md:block font-bold text-gray-900">John Doe</div>
+                  <div class="hidden md:block">&bull;</div>
                   <div>10 hours ago</div>
                   <div>&bull;</div>
                   <div>Category 1</div>
@@ -39,7 +39,7 @@
                </div>
                <div 
                   x-data="{ isOpen:false }"
-                  class="flex items-center space-x-2">
+                  class="flex items-center space-x-2 mt-4 md:mt-0">
                   <div class="bg-gray-200 text-xxs font-semibold uppercase leading-none rounded-full
                      text-center w-28 h-7 px-4 py-2">Open
                   </div>
@@ -57,21 +57,30 @@
                         x-cloak  {{-- addresses this scenario by hiding the element it's attached to until Alpine is fully loaded on the page. --}}
                         x-show.transition.origin.top.left="isOpen" 
                         @click.away = "isOpen=false"
-                        class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                        class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 z-10 md:ml-8 top-8 md:top-6 right-0 md:left-0">
                         <li><a href="#" class="hover:bg-gray-100 px-5 py-3 block transition duration-150 ease-in">Mark As Spam</a></li>
                         <li><a href="#" class="hover:bg-gray-100 px-5 py-3 block transition duration-150 ease-in">Delete Post</a></li>
                      </ul>
                   </button>
                </div>
+
+               <div class="flex items-center md:hidden mt-4 md:mt-0">
+                  <div class="bg-gray-100 text-center rounded-xl h-10 px-4 py-2 pr-8">
+                     <div class="text-sm font-bold leading-none">12</div>
+                     <div class="text-xxs font-semibold leading-none text-gray-400">Votes</div>
+                  </div>
+                  <button class="w-20 bg-gray-200 border border-gray-100 hover:border-gray-400
+                     font-bold text-xxs uppercase transition duration-150 ease-in rounded-xl px-4 py-3 -mx-5">vote
+                  </button>
+               </div>
+
             </div>
-
-
          </div>
       </div>
    </div><!-- ideas container -->
 
    <div class="buttons-container flex items-center mt-3">
-      <div class="flex items-center space-x-4 ml-6">
+      <div class="flex flex-col md:flex-row items-center space-x-4 md:ml-6">
          <div 
             class="relative"
             x-data="{ isOpen: false }"
@@ -89,7 +98,7 @@
                x-cloak  {{-- addresses this scenario by hiding the element it's attached to until Alpine is fully loaded on the page. --}}
                x-show.transition.origin.top.left="isOpen" 
                @click.away = "isOpen=false"
-               class=" absolute z-10 w-104 text-left font-semibold text-sm bg-white shadow-dialog rounded-xl mt-2">
+               class=" absolute z-10 w-64 md:w-104 text-left font-semibold text-sm bg-white shadow-dialog rounded-xl mt-2">
                <form action="#" class="space-y-4 px-4 py-6">
                   <div>
                      <textarea name="post_comment" id="post_comment" cols="30" rows="4"
@@ -97,15 +106,15 @@
                         placeholder="Go ahead, don't be shy. Share your thoughts..."></textarea>
                   </div>
                   
-                  <div class="flex items-center space-x-3">
+                  <div class="flex flex-col md:flex-row items-center md:space-x-3">
                      <button type="button"
-                     class="flex items-center justify-center w-1/2 h-11 text-sm font-semibold bg-blue text-white rounded-xl
+                     class="flex items-center justify-center w-full md:w-1/2 h-11 text-sm font-semibold bg-blue text-white rounded-xl
                           border border-blue hover:bg-blue-hover transition duration-150 ease-in px-8 py-3">
                       Post Comment
                      </button>
                      <button type="button" 
-                           class="flex items-center justify-center w-32 h-11 text-xs font-semibold rounded-xl border 
-                           border-gray-200 hover:bg-gray-400 bg-gray-200 transition duration-150 ease-in px-6 py-3">
+                           class="flex items-center justify-center w-full md:w-32 h-11 text-xs font-semibold rounded-xl border 
+                           border-gray-200 hover:bg-gray-400 bg-gray-200 transition duration-150 ease-in px-6 py-3 mt-2 md:mt-0">
                         <svg class="text-gray-600 w-4 transform -rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
                         </svg>   
@@ -123,8 +132,8 @@
 
             <button type="button" 
                   @click="isOpen = !isOpen"
-                  class="flex items-center justify-center w-32 h-11 text-sm font-semibold rounded-xl border 
-                  border-gray-200 hover:bg-gray-400 bg-gray-200 transition duration-150 ease-in px-4 py-3 ml-3">
+                  class="flex items-center justify-center w-36 h-11 text-sm font-semibold rounded-xl border 
+                  border-gray-200 hover:bg-gray-400 bg-gray-200 transition duration-150 ease-in px-4 py-3 mt-2 md:mt-0">
                <span>Set Status</span>
                <svg class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
@@ -136,7 +145,7 @@
                x-cloak  {{-- addresses this scenario by hiding the element it's attached to until Alpine is fully loaded on the page. --}}
                x-show.transition.origin.top.left="isOpen" 
                @click.away = "isOpen=false"
-               class="absolute z-20 w-76 text-left font-semibold text-sm bg-white shadow-dialog rounded-xl mt-2">
+               class="absolute z-20 w-64 md:w-76 text-left font-semibold text-sm bg-white shadow-dialog rounded-xl mt-2">
                <form action="#" class="space-y-4 px-4 py-6">
                   <div class="space-y-2">
                      <div>
@@ -204,7 +213,7 @@
 
       </div>
       
-      <div class="flex items-center space-x-3">
+      <div class="hidden md:flex items-center space-x-3">
          <div class="bg-white font-semibold text-center rounded-xl px-3 py-2">
             <div class="text-xl leading-snug">12</div>
             <div class="text-gray-400 text-xs leading-none">Votes</div>
@@ -218,16 +227,16 @@
    </div> {{-- end button container --}}
 
 
-   <div class="comments-container relative space-y-6 pt-4 ml-22 my-8 mt-1">
+   <div class="comments-container relative space-y-6 pt-4 md:ml-22 my-8 mt-1">
       <div class="comment-container relative bg-white rounded-xl flex mt-4">
-         <div class="flex flex-1 px-4 py-6">
+         <div class="flex flex-col md:flex-row flex-1 px-4 py-6">
             <div class="flex-none">
                <a href="#">
                   <img src="https://source.unsplash.com/200x200/?face&crop=face&v=3" alt="avatar" class="w-14 h-14 rounded-xl  ">
                </a>
             </div>
             
-            <div class="w-full mx-4">
+            <div class="w-full md:mx-4">
                {{-- <h4 class="text-xl font-semibold">
                   <a href="#" class="hover:underline">A random title can go here</a>
                </h4> --}}
@@ -258,7 +267,7 @@
                            x-cloak  {{-- addresses this scenario by hiding the element it's attached to until Alpine is fully loaded on the page. --}}
                            x-show.transition.origin.top.left="isOpen" 
                            @click.away = "isOpen=false"
-                           class="absolute z-10 w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                           class="absolute z-10 w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 md:ml-8 top-8 md:top-6 right-0 md:left-0">
                            <li><a href="#" class="hover:bg-gray-100 px-5 py-3 block transition duration-150 ease-in">Mark As Spam</a></li>
                            <li><a href="#" class="hover:bg-gray-100 px-5 py-3 block transition duration-150 ease-in">Delete Post</a></li>
                         </ul>
