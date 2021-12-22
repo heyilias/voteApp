5fe8360a26eedc4aee4db95068f8c015befed9fb
heyilias <ilias7576@gmail.com>
2021-12-22 03:01:30 +0100

    Idea Model and Migration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        @livewireStyles
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
@@ -119,5 +120,6 @@
             </div>
             
         </main>
+        @livewireScripts
     </body>
 </html>
